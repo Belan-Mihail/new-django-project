25356a2984c18a1b2107d34b4c9905f964667054
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-02-16 21:38:08 +0000

    commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^$h$us^dfdjbizrc0udp52==5irs@bk^k6suqgxywh$qi%m3$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-belanmihail-newdjangopr-q5a02k77y3n.ws-eu108.gitpod.io', '.herokuapp.com']
 
