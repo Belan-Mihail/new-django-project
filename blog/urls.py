62ee1c96a649ed75818878dfaabc21b551435484
Belan-Mihail Belan <belan_mihail@inbox.ru>
2024-03-05 20:50:14 +0000

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     # path('<str:title>/', views.post_detail, name='post_detail'),  !!
     # ??
     # path('<str:post_title>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete, name='comment_delete'),
 ]
